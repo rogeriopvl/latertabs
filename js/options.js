2580bb02714a157be77c7be32bea767773229aad
rogeriopvl <rogeriopvl@gmail.com>
2013-03-02 14:25:51 +0000

    Added options page.

---
+++ js/options.js
@@ -0,0 +1,12 @@
+function restore_options(){
+    // TODO
+    console.log('Options restored!');
+};
+
+function save_options(){
+    // TODO
+    console.log('Options saved!');
+};
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save_options_button').addEventListener('click', save_options);
